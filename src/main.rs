7378c9b35d67d916bd0431afd90819009ec37300
kynantokoro <59479985+kynantokoro@users.noreply.github.com>
2022-04-02 12:17:32 +0900

    add deps and add libs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use bevy::prelude::*;
+
+#[cfg(feature = "debug")]
+use bevy_inspector_egui::WorldInspectorPlugin;
+
 fn main() {
-    println!("Hello, world!");
+    let mut app = App::new();
+    // Window setup
+    app.insert_resource(WindowDescriptor {
+        title: "Game".to_string(),
+        width: 700.,
+        height: 800.,
+        ..Default::default()
+    })
+    // Bevy default plugins
+    .add_plugins(DefaultPlugins);
+    #[cfg(feature = "debug")]
+    // Debug hierarchy inspector
+    app.add_plugin(WorldInspectorPlugin::new());
+    // Startup system (cameras)
+    app.add_startup_system(camera_setup);
+    // Run the app
+    app.run();
+}
+
+fn camera_setup(mut commands: Commands) {
+    // 2D orthographic camera
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
 }
